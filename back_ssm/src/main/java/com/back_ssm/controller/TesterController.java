4b4011c0a9ca38d2798260555e79b6f4a1695d06
beyond-black-and-beyond-light <13375831+beyond-black-and-beyond-light@user.noreply.gitee.com>
2025-01-01 00:27:57 +0800

    前后联调1

--- back_ssm/src/main/java/com/back_ssm/controller/TesterController.java
+++ back_ssm/src/main/java/com/back_ssm/controller/TesterController.java
@@ -4,17 +4,26 @@ import com.back_ssm.service.TesterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController
 public class TesterController {
     @Autowired
     private TesterService testerService;
-    @RequestMapping(value = "/find_tester", method = RequestMethod.GET)
+//    @RequestMapping(value = "/find_tester", method = RequestMethod.GET)
+//    public String findTester(Model model){
+//        System.out.println("成功");
+//        model.addAttribute("testers",testerService.findTesters());
+//        return  "tester/test_list";
+//    }
+    @GetMapping(value = "/find")
     public String findTester(Model model){
         System.out.println("成功");
-        model.addAttribute("testers",testerService.findTesters());
-        return  "tester/test_list";
-    }
+
+        return  "testers";
+   }
 }
