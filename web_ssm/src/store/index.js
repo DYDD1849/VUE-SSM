9a71e98199533d0dc81e957de2a87250bfad1c73
beyond-black-and-beyond-light <13375831+beyond-black-and-beyond-light@user.noreply.gitee.com>
2024-12-31 22:02:10 +0800

    web_ssm

--- web_ssm/src/store/index.js
+++ web_ssm/src/store/index.js
@@ -1,9 +1,11 @@
 import { createStore } from "vuex";
+ //import user from "@/store/modules/user";
 
 export default createStore({
   state: {},
   getters: {},
-  mutations: {},
+  mutations: {
+  },
   actions: {},
   modules: {},
 });
