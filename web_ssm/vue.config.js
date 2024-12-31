74904f6d2f8b7a8f36bff15baf53bbf4e70de97a
beyond-black-and-beyond-light <13375831+beyond-black-and-beyond-light@user.noreply.gitee.com>
2025-01-01 00:28:06 +0800

    前后联调1

--- web_ssm/vue.config.js
+++ web_ssm/vue.config.js
@@ -1,4 +1,6 @@
-const { defineConfig } = require("@vue/cli-service");
-module.exports = defineConfig({
-  transpileDependencies: true,
-});
+module.exports = {
+  devServer: {
+    port:80,
+    proxy:'http://localhost:8080/', 
+  }
+}
