c9230ef08f987e01b85a376a4f1108a02d4db3f8
dydd1849 <12926140+dydd1849@user.noreply.gitee.com>
2025-01-01 21:42:47 +0800

    first

--- web_ssm/src/App.vue
+++ web_ssm/src/App.vue
@@ -1,73 +1,32 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link>
-
-    <router-link to="/about">About</router-link>
-    <ul>
-      <li v-for="item in backdata" :key="item.testId">
-        {{ item.testId }}
-      </li>
-    </ul>
-  </nav>
-
-  <router-view />
-
-  <el-button type="primary" @click="clickpost">主要按钮</el-button>
+  <div>
+    first one
+  </div>
 </template>
 <script>
-import { test } from '@/api/test.js'
-// import axios from "axios"
-import { data } from '@/api/data.js'
 export default {
   name: 'App',
   data() {
     return {
-      a: { "testId": 3 },
-      backdata: null,
     }
   },
   methods: {
-    clickpost: function () {
+    // clickpost: function () {
 
-      // let data = {"testId":1}  // 带发送的数据
-      // axios.post('http://localhost:80/back_ssm/data', this.a).then((res) => {
-      //   console.log(res)
-      // }, (error) => {
-      //   console.log(error)
-      // })
-      data(this.a).then((res) => {
-        console.log(res)
-      }, (error) => {
-        console.log(error)
-      })
-    }
+    //   // let data = {"testId":1}  // 带发送的数据
+    //   // axios.post('http://localhost:80/back_ssm/data', this.a).then((res) => {
+    //   //   console.log(res)
+    //   // }, (error) => {
+    //   //   console.log(error)
+    //   // })
+    //   data(this.a).then((res) => {
+    //     console.log(res)
+    //   }, (error) => {
+    //     console.log(error)
+    //   })
+    // }
   },
   mounted() {
-    test().then(res => {
-      this.backdata = res.data;
-    })
   }
 }
 </script>
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
