127ef4e27486cbee3d919058ea1f14e8f1ed460b
beyond-black-and-beyond-light <13375831+beyond-black-and-beyond-light@user.noreply.gitee.com>
2025-01-05 21:15:57 +0800

    管理员页面3

--- web_ssm/src/api/main/manager/CourseTable.js
+++ web_ssm/src/api/main/manager/CourseTable.js
@@ -1,11 +1,11 @@
 import request from "@/api";
 //新增用户
 //查询用户列表
-export function StudentCourseTable(query){
+export function CourseTable(query){
     
     return request({
         //发送请求
-        url:'/findAllStudentCourse',
+        url:'/findAllCourse',
         method:'post',
         data:query
     })
