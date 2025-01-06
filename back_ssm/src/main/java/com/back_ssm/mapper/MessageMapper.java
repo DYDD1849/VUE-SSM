21ac7d6034c1238320e4de2c0ccad0e3ef488464
dydd1849 <12926140+dydd1849@user.noreply.gitee.com>
2025-01-06 19:15:09 +0800

    消息接口

--- back_ssm/src/main/java/com/back_ssm/mapper/MessageMapper.java
+++ back_ssm/src/main/java/com/back_ssm/mapper/MessageMapper.java
@@ -3,6 +3,7 @@ package com.back_ssm.mapper;
 import com.back_ssm.pojo.Message;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import java.util.List;
 
@@ -13,4 +14,8 @@ public interface MessageMapper {
     public List<Message> findMessageBySenderReceiver2(Message message);
     @Insert("insert into chat values(#{sender},#{receiver},#{msg},#{date},#{isread})")
     public int addMessage(Message message);
+    @Select("select * from chat where isread='false' and receiver=#{receiver} ")
+    public List<Message> findIsReadMessageByReceiver(Message message);
+    @Update("update chat set isread='true' where receiver=#{receiver}")
+    public int UpdateMessageIsReadByReceiver(Message message);
 }
--- back_ssm/src/main/java/com/back_ssm/service/impl/MessageServiceImpl.java
+++ back_ssm/src/main/java/com/back_ssm/service/impl/MessageServiceImpl.java
@@ -6,6 +6,8 @@ import com.back_ssm.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.List;
 @Service
 public class MessageServiceImpl implements MessageService {
@@ -14,13 +16,28 @@ public class MessageServiceImpl implements MessageService {
     @Override
     public List<Message> findMessageBySenderReceiver(Message message) {
         List<Message> merge= messageMapper.findMessageBySenderReceiver(message);
+        messageMapper.UpdateMessageIsReadByReceiver(message);
         merge.addAll(messageMapper.findMessageBySenderReceiver2(message));
         return merge;
     }
 
     @Override
     public int addMessage(Message message) {
+
+        Timestamp now = new Timestamp(System.currentTimeMillis());
+
+        // 创建一个SimpleDateFormat对象来格式化日期时间
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        // 格式化Timestamp对象
+        String formattedDateTime = sdf.format(now);
         message.setIsread("false");
+        message.setDate(formattedDateTime);
         return messageMapper.addMessage(message);
+    }
+
+    @Override
+    public List<Message> findIsReadMessageByReceiver(Message message) {
+        return messageMapper.findIsReadMessageByReceiver(message);
     }
 }
